17663e3673da4b25eea6d75f2b64c8a4a7a7f139
ZIN-THI-THI-TUN <zin.thi.thi.tun@airitech.co.jp>
2021-08-19 17:00:35 +0900

    v-slotについて

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-new Vue({
-    el:'#app',
-    data(){
-        return{
-            text:"Hello World!",
-            body:"Let's Start Vue..."
-        };
+Vue.component('blog-slot-post', {
+    template: '<p>この人の名前は<slot></slot>です。</p>'
+  })
+  
+Vue.component('blog-slot-info', {
+    template: `
+                <ul>
+                  <li>名前：<slot>名無しの権兵衛</slot></li>
+                  <li>年齢：<slot name="age">記入なし</slot></li>
+                  <li>性別：<slot name="sex">不明</slot></li>
+                </ul>
+                `
+  })
+  
+Vue.component('blog-slot-user', {
+    data() {
+    return{
+      user:{
+        firstName: 'John',
+        lastName: 'Doe',
+        age: '25',
+        sex: '男性',
+      },
     }
-})
+  },
+    template: `
+                <span>
+                  User Last Name is <slot>{{ user.lastName }}.</slot>
+                </span>
+                `
+  })
+  
+new Vue({
+    el: '#slot-demo'
+  })
